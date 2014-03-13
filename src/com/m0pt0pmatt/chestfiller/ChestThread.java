410dc81768c395cd2762e97e1738eaa18b8bf0b4
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2014-03-13 12:18:53 -0600

    fixes

--- src/com/m0pt0pmatt/chestfiller/ChestThread.java
+++ src/com/m0pt0pmatt/chestfiller/ChestThread.java
@@ -14,12 +14,28 @@ public class ChestThread extends Thread{
 			}
 		}
 		
-		if (Bukkit.getWorld("Wilderness").getPopulators().size() <= 1){
-			
-			
-			System.out.println("Added populator!!!!!!!!!!!!!!!");
+		Bukkit.getWorld("Wilderness").setMonsterSpawnLimit(600);
+		Bukkit.getWorld("Wilderness").setTicksPerMonsterSpawns(1);
+		Bukkit.getWorld("Wilderness").setTicksPerAnimalSpawns(1);
+		Bukkit.getWorld("Wilderness").setAnimalSpawnLimit(600);
+		
+		if (Bukkit.getWorld("Wilderness").getPopulators().size() <= 1){	
 			Bukkit.getWorld("Wilderness").getPopulators().add(new ChestPopulator());
-	
+			
+		}
+		
+		while(true){
+			Bukkit.getWorld("Wilderness").setTime(15000);
+			Bukkit.getWorld("Wilderness").setMonsterSpawnLimit(600);
+			Bukkit.getWorld("Wilderness").setTicksPerMonsterSpawns(1);
+			Bukkit.getWorld("Wilderness").setTicksPerAnimalSpawns(1);
+			Bukkit.getWorld("Wilderness").setAnimalSpawnLimit(600);
+			try {
+				Thread.sleep(20000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
 		}
 	}	
 	
